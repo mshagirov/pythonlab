d1c1809107f2125fd3587066adb987f82a361469
mshagirov <muratshagirov@gmail.com>
2025-05-07 23:43:20 +0800

    site root

---
+++ docs/main.py
@@ -0,0 +1,13 @@
+import math
+import random
+
+def main():
+    print(f"Cos(pi) = {math.cos(math.pi)}; e^1 = {math.exp(1)};")
+    
+    numbers = [random.random() for _ in range(10)]
+    print(numbers)
+
+# if __name__ == '__main__':
+main()
+
+print('hello from main.py')
